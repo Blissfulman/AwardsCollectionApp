00a00517c47185c2c0abb497bdce7d937b754ce5
Evgeny Novgorodov <greatly01@ya.ru>
2020-12-28 22:18:26 +0500

    Initial Commit

---
+++ AwardsCollectionApp/AwardsCollectionApp.swift
@@ -0,0 +1,18 @@
+//
+//  AwardsCollectionApp.swift
+//  AwardsCollectionApp
+//
+//  Created by Alexey Efimov on 01.11.2020.
+//  Copyright © 2020 Alexey Efimov. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabViews()
+        }
+    }
+}
